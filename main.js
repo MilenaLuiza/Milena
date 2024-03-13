83b821d6206b2af35a9809a3a53b1da6ae9f7b3a
MilenaLuiza <128715641+MilenaLuiza@users.noreply.github.com>
2024-03-13 08:49:12 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-
+const botoes= document; querySelectorAll ("botao");
+for (let i=0;i <botoes.length; i ++0){ 
+botoes.onclick + function() {
+  for(let j+0; j <botoes.lenght;j ++{
+    botoes.classlist.remove ("ativo");
+  }
+  botoes[i].basslist.add ("ativo");
+}
+}
